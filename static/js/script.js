cade739d1ebcad6f2286984dcab751a265676758
Harry-Leepz <hdhillon478@gmail.com>
2021-04-20 16:11:41 +0000

    Add JS functions to add animation to page heading texts, CSS styling added to change color with animation

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+let animation = document.querySelector('.animation');
+let animationText = animation.textContent;
+let animationTextSplit = animationText.split("");
+
+animation.textContent = ""
+
+for (let i = 0; i < animationTextSplit.length; i++) {
+    animation.innerHTML += "<span>" + animationTextSplit[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+    let span = animation.querySelectorAll('span')[char];
+    span.classList.add('red-animation');
+    char++
+    if(char === animationTextSplit.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
