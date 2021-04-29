f0ab62ec57e1b5d0b26b97c2b3bf03299c34deda
Harry-Leepz <hdhillon478@gmail.com>
2021-04-29 16:59:37 +0000

    Fix grammar errors on nike and jordan brand pages

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
+
+
 if os.path.exists("env.py"):
     import env
 
